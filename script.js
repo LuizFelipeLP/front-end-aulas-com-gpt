f0c7641163a905c86adc744e41a877369a7b9f74
Luiz Felipe Pereira de Lima <150875264+LuizFelipeLP@users.noreply.github.com>
2025-10-27 14:17:46 +0000

    correção de erro da mudança de frases

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ botao.addEventListener("click", () => {
     const corAleatoria = "#" + Math.floor(Math.random() * 16777215).toString(16);
     corpo.style.backgroundColor = corAleatoria;
 
-    const tituloAleatorio = "#" + Math.floor(Math.random(cinema, cachorro));
-    titulo.textContent = tituloAleatorio;
+    const frases = [
+        "To desesperado",
+        "Muito medo",
+        "Falta de ar",
+        "Conseguirei?",
+        "Tranquilo"
+    ];
+    const indice = Math.floor(Math.random() * frases.length);
+    titulo.textContent = frases[indice];
 })
